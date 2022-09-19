bf84864139450483b823e6b2e91efb7236e4676f
Viacheslav-ua <atph@ukr.net>
2022-09-19 20:44:59 +0300

    lesson-#9

--- src/app/components/child/child.component.ts
+++ src/app/components/child/child.component.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @angular-eslint/no-empty-lifecycle-method */
 /* eslint-disable @angular-eslint/no-conflicting-lifecycle */
 import {
   AfterContentChecked,
@@ -33,39 +34,39 @@ export class ChildComponent implements
   title = ''
 
   constructor() {
-    console.log('ChildComponent.constructor');
+    // console.log('ChildComponent.constructor');
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    console.log('ChildComponent.OnChanges', changes);
+    // console.log('ChildComponent.OnChanges', changes);
 
   }
   ngOnInit(): void {
-    console.log('ChildComponent.ngOnInit');
+    // console.log('ChildComponent.ngOnInit');
 
   }
   ngDoCheck(): void {
-    console.log('ChildComponent.DoCheck');
+    // console.log('ChildComponent.DoCheck');
 
   }
   ngAfterContentInit(): void {
-    console.log('ChildComponent.AfterContentInit');
+    // console.log('ChildComponent.AfterContentInit');
 
   }
   ngAfterContentChecked(): void {
-    console.log('ChildComponent.AfterContentChecked');
+    // console.log('ChildComponent.AfterContentChecked');
 
   }
   ngAfterViewInit(): void {
-    console.log('ChildComponent.AfterViewInit');
+    // console.log('ChildComponent.AfterViewInit');
 
   }
   ngAfterViewChecked(): void {
-    console.log('ChildComponent.AfterViewChecked');
+    // console.log('ChildComponent.AfterViewChecked');
 
   }
   ngOnDestroy(): void {
-    console.log('ChildComponent.OnDestroy');
+    // console.log('ChildComponent.OnDestroy');
 
   }
 }
--- src/app/components/parent/parent.component.ts
+++ src/app/components/parent/parent.component.ts
@@ -12,6 +12,7 @@ import {
   OnInit,
   SimpleChanges
 } from '@angular/core';
+import { fromEvent, Subject, Subscription, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-parent',
@@ -21,52 +22,72 @@ import {
 export class ParentComponent implements
   OnChanges,
   OnInit,
-  DoCheck,
-  AfterContentInit,
-  AfterContentChecked,
-  AfterViewInit,
-  AfterViewChecked,
+  // DoCheck,
+  // AfterContentInit,
+  // AfterContentChecked,
+  // AfterViewInit,
+  // AfterViewChecked,
   OnDestroy
   {
 
   @Input()
   title = 'before init parent'
 
+  subscription?: Subscription
+  subscriptions: Subscription[] = []
+  destroy$ = new Subject()
+
   constructor() {
     console.log('ParentComponent.constructor', this.title);
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    console.log('ParentComponent.OnChanges', changes, this.title);
 
+    console.log('ParentComponent.OnChanges', changes, this.title);
+    // this.subscription = fromEvent(document, 'click').subscribe(console.log)
+
+    // this.subscriptions.push(fromEvent(document, 'click').subscribe(console.log))
+
+    fromEvent(document, 'click').pipe(
+      takeUntil(this.destroy$)
+    ).subscribe(console.log)
   }
   ngOnInit(): void {
     console.log('ParentComponent.ngOnInit', this.title);
 
   }
-  ngDoCheck(): void {
-    console.log('ParentComponent.DoCheck', this.title);
+  // ngDoCheck(): void {
+  //   console.log('ParentComponent.DoCheck', this.title);
 
-  }
-  ngAfterContentInit(): void {
-    console.log('ParentComponent.AfterContentInit', this.title);
+  // }
+  // ngAfterContentInit(): void {
+  //   console.log('ParentComponent.AfterContentInit', this.title);
 
-  }
-  ngAfterContentChecked(): void {
-    console.log('ParentComponent.AfterContentChecked', this.title);
+  // }
+  // ngAfterContentChecked(): void {
+  //   console.log('ParentComponent.AfterContentChecked', this.title);
 
-  }
-  ngAfterViewInit(): void {
-    console.log('ParentComponent.AfterViewInit', this.title);
+  // }
+  // ngAfterViewInit(): void {
+  //   console.log('ParentComponent.AfterViewInit', this.title);
 
-  }
-  ngAfterViewChecked(): void {
-    console.log('ParentComponent.AfterViewChecked', this.title);
+  // }
+  // ngAfterViewChecked(): void {
+  //   console.log('ParentComponent.AfterViewChecked', this.title);
 
-  }
+  // }
   ngOnDestroy(): void {
     console.log('ParentComponent.OnDestroy', this.title);
+    // this.subscription?.unsubscribe()
 
+    // for (let s of this.subscriptions) {
+    //   s.unsubscribe()
+    // }
+
+    // this.subscriptions.forEach(s => s.unsubscribe())
+
+    this.destroy$.next(true)
+    this.destroy$.complete()
   }
 
 }
